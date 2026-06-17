5c519fd1cdfca0513f2ae0312b8510ca8f3509b6
unknown <bobertbohemianbobux@gmail.com>
2026-06-17 14:43:44 -0300

    booyah

---
+++ src/test/java/com/example/projecoN/exercicioN/ExercicioNApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projecoN.exercicioN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
