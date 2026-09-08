43fd394854e5ce80791c9829f4c3b9ed49378c36
unknown <bobertbohemianbobux@gmail.com>
2026-09-08 17:45:18 -0300

    cadastroMatricula

--- src/main/java/com/example/projecoN/exercicioN/Controller/Matriculacontroller.java
+++ src/main/java/com/example/projecoN/exercicioN/Controller/Matriculacontroller.java
@@ -1,22 +1,46 @@
 package com.example.projecoN.exercicioN.Controller;
 
+import com.example.projecoN.exercicioN.Service.AlunoService;
+import com.example.projecoN.exercicioN.Service.CursoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.projecoN.exercicioN.DTO.MatriculaDTO;
 import com.example.projecoN.exercicioN.Service.MatriculaService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller 
-@RequestMapping ("/matriculaCTR")
+@Controller
+@RequestMapping("/matriculaCTR")
 public class Matriculacontroller {
+    private final CursoService cursoService;
+    private final AlunoService alunoService;
     private final MatriculaService matriculaService;
-    private final Cursocontroller cursocontroller;
-    private final Alunocontroller alunocontroller;
+    
+
     public Matriculacontroller(MatriculaService matriculaService,
-         Cursocontroller cursocontroller,
-          Alunocontroller alunocontroller){
-    this.matriculaService = matriculaService;
-    this.alunocontroller = alunocontroller;
-    this.cursocontroller = cursocontroller;
+            Cursocontroller cursocontroller,
+            Alunocontroller alunocontroller, AlunoService oAlunoService, CursoService oCursoService) {
+        this.matriculaService = matriculaService;
+        this.cursoService = oCursoService;
+        this.alunoService = oAlunoService;
+
+    }
+
+    @GetMapping("/listarMatriculas")
+    public String listMatricula(Model oModel) {
+        oModel.addAttribute("matriculas", matriculaService.listarTodasMatriculas());
+        return "listarMatricula";
+    }
+
+    @GetMapping("/formCadastrarM")
+    public String showFormCadastrarMatricula(Model oModel) {
+        oModel.addAttribute("MariculaDTO", new MatriculaDTO());
+        oModel.addAttribute("listAlunos", alunoService.listarTodosAlunos());
+        oModel.addAttribute("listCursos", cursoService.listarTodosCursos());
+
+        return "cadastrarMatricula";
     }
 
 }
